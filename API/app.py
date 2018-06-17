49115c307e7192f2762f8ea6a073dfde388669b1
Pierre <pierre.springer@tum.de>
2018-06-17 22:14:11 +0200

    Added torinit and made 24h cache working

--- API/app.py
+++ API/app.py
@@ -1,38 +1,39 @@
 from flask import Flask
 from flask_restful import reqparse, abort, Api, Resource
+import tbselenium.common as cm
+from tbselenium.tbdriver import TorBrowserDriver
+import cachetools.func
+import numpy as np
 
 app = Flask(__name__)
 api = Api(app)
 
-LANGUAGE_SUPPORT = ["de", "en", "en-US", "ru"]
+LANGUAGE_SUPPORT = ["de", "en", "en-US", "ru"]  # Requested languages, just an abstract for now
 REQUESTEDLANGUAGE = []
 
+parser = reqparse.RequestParser()
+parser.add_argument('lang')
 
-def Mobiledata(lang):
+
+@cachetools.func.ttl_cache(maxsize=6400, ttl=60 * 60 * 24)
+def FakeHeader(lang):
     # Here we need to pass a dict, which will be made to a json response containing
-    # the user agend, encoding and language which fits perfectly
+    # the user agent, encoding and language which fits perfectly
 
     # 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/66.0.3359.181 Safari/537.36';
-    accept_encoding = ""
-    user_agent = ""
-    accept = ""
-    accept_lang = "en en_US;q=0.5"  # lang
-    newheader = {"accept_encoding": accept_encoding, "accept": accept,
-                 "user_agent": user_agent, "accept_lang": accept_lang}
-    return newheader
-
-
-def Browserdata(lang):
-    # Here we need to pass a dict, which will be made to a json response containing
-    # the user agend, encoding and language which fits perfectly
-
-    # 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/66.0.3359.181 Safari/537.36';
-    accept_encoding = ""
-    user_agent = ""
-    accept = ""
-    accept_lang = "en en_US;q=0.5"  # lang
-    newheader = {"accept_encoding": accept_encoding, "accept": accept,
-                 "user_agent": user_agent, "accept_lang": accept_lang}
+    # driver = TorBrowserDriver("tor-browser_en-US/", tor_cfg=cm.USE_RUNNING_TOR, socks_port=9150)
+    # driver.get('https://check.torproject.org')
+    # driver.load_url("https://check.torproject.org", wait_on_page=3)
+    # print(driver.find_element_by("h1.on").text)
+    test = np.random.normal(1000, 5, 1)
+    test = np.round(test)
+    accept_encoding = "gzip, deflate, br" + str(test)
+    user_agent = "Mozilla/5.0 (Windows NT 6.1; rv:52.0) Gecko/20100101 Firefox/52.0"
+    accept = "text/html, */*; q=0.01"
+    accept_lang = "en-US,en;q=0.5"  # lang
+    newheader = {"accept_encoding": accept_encoding,
+                 "user_agent": user_agent, "accept_lang": accept_lang,
+                 "accept_code": accept}
     return newheader
 
 
@@ -41,16 +42,12 @@ def abort_if_invalid_country(lang):
         abort(410, message="Language is not supported so far")
 
 
-parser = reqparse.RequestParser()
-parser.add_argument('lang')
-
-
 # Mobile Area
 class MobileRequest(Resource):
 
     def get(self, lang):
         abort_if_invalid_country(lang)
-        return Mobiledata(lang)
+        return FakeHeader(lang)
 
     # def put(self, lang):
     # Not needed so far
@@ -63,12 +60,11 @@ class BrowserRequest(Resource):
 
     def get(self, lang):
         abort_if_invalid_country(lang)
-        return Browserdata(lang)
+        return FakeHeader(lang)
 
 
 # shows a list of all supported languages
 class Supported_languages(Resource):
-
     def get(self):
         return LANGUAGE_SUPPORT
 
--- chrome_plugin/background.js
+++ chrome_plugin/background.js
@@ -3,7 +3,6 @@ chrome.webRequest.onBeforeSendHeaders.addListener(
         //alert(JSON.stringify(info));
         // Replace the User-Agent header
         var headers = info.requestHeaders;
-        //headers.delete("X-DevTools-Emulate-Network-Conditions-Client-Id");
         var req = new XMLHttpRequest();
         req.open(
                 "GET",
@@ -16,7 +15,7 @@ chrome.webRequest.onBeforeSendHeaders.addListener(
         //}
         req.send(null);
         var resp = JSON.parse(req.responseText); // Parse to get json object, and avoid attacks
-        // alert(JSON.stringify(resp["user_agent"]));
+        alert(JSON.stringify(resp));
         headers.forEach(function(header, i) {
             if (header.name.toLowerCase() == 'user-agent') { 
                 header.value = JSON.stringify(resp["user_agent"]);
@@ -26,7 +25,7 @@ chrome.webRequest.onBeforeSendHeaders.addListener(
                 header.value = JSON.stringify(resp["accept"]);
             }
             if (header.name.toLowerCase() == 'accept-language') { 
-                header.value = 'en en_US;q=0.5';
+                header.value = '';
             }
             if (header.name.toLowerCase() == 'accept-encoding') { 
                 header.value = JSON.stringify(resp["accept_encoding"]);
@@ -44,8 +43,6 @@ chrome.webRequest.onBeforeSendHeaders.addListener(
         urls: [
             "https://www.whoishostingthis.com/*",
             "http://127.0.0.1:6789/*",
-            "http://www.useragentstring.com/index.php",
-            "http://www.useragentstring.com/*",
             "https://panopticlick.eff.org/*",
             "<all_urls>"
         ],
