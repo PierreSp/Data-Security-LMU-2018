037a7b08171012ae7a7906944acce84aa6d7aa10
Pierre <pierre.springer@tum.de>
2018-07-01 23:55:52 +0200

    disabled for testing

--- chrome-plugin-v2/js/agent_faker.js
+++ chrome-plugin-v2/js/agent_faker.js
@@ -3,36 +3,36 @@ chrome.webRequest.onBeforeSendHeaders.addListener(
         //alert(JSON.stringify(info));
         // Replace the User-Agent header
         var headers = info.requestHeaders;
-        var req = new XMLHttpRequest();
-        req.open(
-                "GET",
-                "http://localhost:1991/browserreq/de", false);
-        //req.onreadystatechange = function() {
-        //  if (req.readyState == 4) {
-            // Wait till done
-        //    var resp = JSON.parse(req.responseText);
-        //  }
-        //}
-        req.send(null);
+        // var req = new XMLHttpRequest();
+        // req.open(
+        //         "GET",
+        //         "http://localhost:1991/browserreq/de", false);
+        // //req.onreadystatechange = function() {
+        // //  if (req.readyState == 4) {
+        //     // Wait till done
+        // //    var resp = JSON.parse(req.responseText);
+        // //  }
+        // //}
+        // req.send(null);
         var resp = JSON.parse(req.responseText); // Parse to get json object, and avoid attacks
         
         headers.forEach(function(header, i) {
             if (header.name.toLowerCase() == 'user-agent') { 
-                alert(header.value );
-                header.value = JSON.stringify(resp["user_agent"]);
-                alert(header.value );
+                // header.value = JSON.stringify(resp["user_agent"]);
+                header.value = "Mozilla/5.0 (Windows NT 6.1; rv:52.0) Gecko/20100101 Firefox/52.0";
             }
             if (header.name.toLowerCase() == 'accept') { 
-                //alert("changing");
-                header.value = JSON.stringify(resp["accept"]);
-                alert(header.value );
+                //alert("changing"); text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8
+                // header.value = JSON.stringify(resp["accept"]);
+                header.value = "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8";
+    
             }
             if (header.name.toLowerCase() == 'accept-language') { 
                 header.value = '';
             }
             if (header.name.toLowerCase() == 'accept-encoding') { 
-                header.value = JSON.stringify(resp["accept_encoding"]);
-                alert(header.value );
+                // header.value = JSON.stringify(resp["accept_encoding"]);
+                header.value = '';
             }
             if (header.name.toLowerCase() == 'referer') { 
                 header.value = '';
